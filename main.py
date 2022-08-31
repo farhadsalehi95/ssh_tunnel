0554317ed2af80d10339e9b99ec1700e7547ff6c
Farhad Salehi <f.salehi@fsalehi.ldc.local>
2022-08-31 18:41:41 +0430

    first commit

--- main.py
+++ main.py
@@ -9,22 +9,40 @@ import json
 import argparse
 import ipaddress
 
+def getArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s' , '--sip' , dest = 'source_ip' , nargs='+',  help="add source ip address you want allow on ufw like this 192.168.1.1 192.168.2.2 192.168.3.3 and you can add 0.0.0.0 for any")
+    parser.add_argument('-sp' , '--sport' , dest = 'source_port' , nargs='+' , help='add source port you want allow on ufw like this 3306 3389 22')
+    parser.add_argument('-d' , '--dip' , dest = 'destination_ip' , nargs='+' , help="add destination ip address you want allow on ufw like this 192.168.1.1 192.168.2.2 192.168.3.3 and you can add 0.0.0.0 for any")
+    parser.add_argument('-dp' , '--dport' , dest = 'destination_port' , nargs='+' , help='add destination port you want allow on ufw like this 3306 3389 22')
+    return parser.parse_args()
 
+def ipValidation(args):
+    try:
+        for i in args:
+            try:
+                ipaddress.ip_address(i)
+            except ValueError:
+                print(f'IP {i} is not valid')
+    except:
+        pass
 
-parser = argparse.ArgumentParser()
+def portValidation(args):
+    try:
+        for i in args:
+            try:
+                if int(i) > 65535:
+                    print (f'port {i} is greater than 65535')
+                elif not int(i.isdigit()):
+                    print (f'please input number for port not {i}')
+            except:
+                pass
+    except:
+        pass
 
-parser.add_argument('-s' , '--sip' , dest = 'source_ip' , nargs='+',  help="add source ip address you want allow on ufw like this 192.168.1.1 192.168.2.2 192.168.3.3 and you can add 0.0.0.0 for any")
-parser.add_argument('-sp' , '--sport' , dest = 'source_port' , nargs='+' , help='add source port you want allow on ufw like this 3306 3389 22')
-parser.add_argument('-d' , '--dip' , dest = 'destination_ip' , nargs='+' , help="add destination ip address you want allow on ufw like this 192.168.1.1 192.168.2.2 192.168.3.3 and you can add 0.0.0.0 for any")
-parser.add_argument('-dp' , '--dport' , dest = 'destination_port' , nargs='+' , help='add destination port you want allow on ufw like this 3306 3389 22')
+args = getArgs()
+ipValidation(args.source_ip)
+ipValidation(args.destination_ip)
+portValidation(args.source_port)
+portValidation(args.destination_port)
 
-args = parser.parse_args()
-
-input_sips = args.source_ip
-input_dips = args.destination_ip
-input_sport = args.source_port
-input_dport = args.destination_port
-
-for a  in input_sips:
-    ipaddress
-ip_address_dictionary = {"ernyka_respina": "46.209.212.16/28" , "ernyka_afranet" : "217.11.27.194" , "ernyka_hetzner" : "162.55.76.101" }
